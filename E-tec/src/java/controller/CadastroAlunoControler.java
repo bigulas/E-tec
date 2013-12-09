b3a8e6e94b721abfe4dfac43ec510a3293bc3d26
ArnaldoGuedes <>
2013-12-09 20:12:41 -0200

    Melhoria no Controller de Cadastro Aluno

--- E-tec/src/java/controller/CadastroAlunoControler.java
+++ E-tec/src/java/controller/CadastroAlunoControler.java
@@ -3,12 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controller;
 
 import domainModel.Aluno;
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import negocio.AlunoBO;
 
 /**
  *
@@ -18,13 +21,41 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class CadastroAlunoControler {
 
- private Aluno alunoCadastrar;
+    private Aluno alunoCadastrar;
+    private AlunoBO alunoBO = new AlunoBO();
 
     public CadastroAlunoControler() {
     }
+
+    //MEUS METODOS
+    public void iniciarTela() throws IOException {
+        this.alunoCadastrar = new Aluno();
+        FacesContext.getCurrentInstance().getExternalContext().redirect("faces/cadastroaluno.xhtml");
+    }
+
+    public void mensagemSucesso(String msg) {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, msg);
+        fc.addMessage(null, facesMessage);
+    }
     
+    public void mensagemErro(String msg) {
+        FacesContext fc = FacesContext.getCurrentInstance();
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, msg);
+        fc.addMessage(null, facesMessage);
+    }
+
     
-    
+    public void salvar() {
+        try {
+            alunoBO.adicionar(alunoCadastrar);
+            mensagemSucesso("Cadastrado com Sucesso!");
+        } catch (Exception e) {
+            mensagemErro(e.getMessage());
+        }
+
+    }
+
     //GETRS
     public Aluno getAlunoCadastrar() {
         return alunoCadastrar;
@@ -34,6 +65,5 @@ public class CadastroAlunoControler {
     public void setAlunoCadastrar(Aluno alunoCadastrar) {
         this.alunoCadastrar = alunoCadastrar;
     }
-    
-    
+
 }
